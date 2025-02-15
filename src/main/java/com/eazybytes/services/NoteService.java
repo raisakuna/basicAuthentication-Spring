b884a1b7da50e3615f06626979eaadf6c93beeeb
raisakuna <sakunaxl@gmail.com>
2025-02-14 20:23:48 -0500

    dependency lombook and jpa added, service, repository and model class added

---
+++ src/main/java/com/eazybytes/services/NoteService.java
@@ -0,0 +1,15 @@
+package com.eazybytes.services;
+
+import com.eazybytes.model.Note;
+
+import java.util.List;
+
+public interface NoteService {
+    Note createNoteForUser(String username, String content);
+
+    Note updateNoteForUser(Long noteId, String content, String username);
+
+    void deleteNoteForUser(Long noteId, String username);
+
+    List<Note> getNotesForUser(String username);
+}
